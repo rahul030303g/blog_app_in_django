4c34c9bb7f16ff6979dcbb8c7d885ff2d85bfcf9
rahulsingh <rahulsingh10041998@gmail.com>
2023-03-13 14:27:30 +0530

    folder structure changed

---
+++ blog_content/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Post
+from django.views import generic
+from django.views.generic.detail import DetailView
+
+
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    
+    
+    
